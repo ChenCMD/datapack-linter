44be38d6c79d5d136d151cd3b63e249e4892cc12
ChenCMD <scnmed.g@gmail.com>
2021-02-02 16:10:36 +0900

    add test

--- scripts/index.ts
+++ scripts/index.ts
@@ -74,6 +74,7 @@ async function lint() {
     // message output
     const failCount = outputMessage(results);
 
+    console.log(process.env['RUNNER_DEBUG']);
     // last message output
     if (failCount.error + failCount.warning === 0) {
         core.info('Check successful');
