23b60a4fbce76abc1a74bad98c07afe1ece7209a
ChenCMD <scnmed.g@gmail.com>
2021-02-03 17:48:39 +0900

    Fix not work dataVersion config

--- scripts/index.ts
+++ scripts/index.ts
@@ -1,7 +1,7 @@
-import { getClientCapabilities, isRelIncluded, Uri } from '@spgoding/datapack-language-server/lib/types';
+import { getClientCapabilities, isRelIncluded, Uri, VersionInformation } from '@spgoding/datapack-language-server/lib/types';
 import { getTextDocument, walkFile } from '@spgoding/datapack-language-server/lib/services/common';
 import { PluginLoader } from '@spgoding/datapack-language-server/lib/plugins/PluginLoader';
-import { DatapackLanguageService, readFile } from '@spgoding/datapack-language-server';
+import { DatapackLanguageService, readFile, requestText } from '@spgoding/datapack-language-server';
 import { IdentityNode } from '@spgoding/datapack-language-server/lib/nodes';
 import { loadLocale } from '@spgoding/datapack-language-server/lib/locales';
 import * as core from '@actions/core';
@@ -29,7 +29,8 @@ async function lint() {
         capabilities,
         fetchConfig,
         globalStoragePath: path.join(dir, '_storage'),
-        plugins: await PluginLoader.load()
+        plugins: await PluginLoader.load(),
+        versionInformation: await getLatestVersions()
     });
     service.init();
     const dirUri = Uri.file(dir);
@@ -114,4 +115,48 @@ async function fetchConfig() {
     const config = await getConfiguration(configUri.fsPath);
     await loadLocale(config.env.language, 'en');
     return config;
+}
+
+/**
+ * @license
+ * MIT License
+ *
+ * Copyright (c) 2019-2020 SPGoding
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+async function getLatestVersions() {
+    let ans: VersionInformation | undefined;
+    try {
+        console.info('[LatestVersions] Fetching the latest versions...');
+        const str = await Promise.race([
+            requestText('https://launchermeta.mojang.com/mc/game/version_manifest.json'),
+            new Promise<string>((_, reject) => setTimeout(() => reject(new Error('Time out!')), 7_000))
+        ]);
+        const { latest: { release, snapshot }, versions }: { latest: { release: string, snapshot: string }, versions: { id: string }[] } = JSON.parse(str);
+        const processedVersion = '1.16.2';
+        const processedVersionIndex = versions.findIndex(v => v.id === processedVersion);
+        const processedVersions = processedVersionIndex >= 0 ? versions.slice(0, processedVersionIndex + 1).map(v => v.id) : [];
+        ans = (release && snapshot) ? { latestRelease: release, latestSnapshot: snapshot, processedVersions } : undefined;
+    } catch (e) {
+        console.warn(`[LatestVersions] ${e}`);
+    }
+    console.info(`[LatestVersions] versionInformation = ${JSON.stringify(ans)}`);
+    return ans;
 }
