577c3e872c8159453016ccd9cff58e9524884068
ChenCMD <scnmed.g@gmail.com>
2021-05-09 02:28:00 +0900

    Does not work properly with Re-run jobs
    Fix #7

--- scripts/wrapper/actions.ts
+++ scripts/wrapper/actions.ts
@@ -2,7 +2,7 @@ import { context } from '@actions/github';
 import * as cache from '@actions/cache';
 import * as core from '@actions/core';
 
-const cachedfiles = ['.cache'];
+const cachedFiles = ['.cache'];
 const privateCacheVersion = 1;
 
 export function isCommitMessageIncluded(str: string): boolean {
@@ -31,7 +31,11 @@ export function getActionInput(type: 'string' | 'string[]' | 'number' | 'boolean
 export async function tryRestoreCache(cacheVersion: number): Promise<boolean> {
     if (!context.payload.commits) return false;
     try {
-        return !!await cache.restoreCache(cachedfiles, '', [getCacheKeyPrefix(cacheVersion)]);
+        if (await cache.restoreCache(cachedFiles, getCacheKey(cacheVersion, context.runId))) {
+            core.info('Cache is not used because of the re-run jobs.');
+            return false;
+        }
+        return !!await cache.restoreCache(cachedFiles, '', [getCacheKey(cacheVersion)]);
     } catch (e) {
         core.warning('Failed to load the cache. The following errors may be resolved by reporting them in the datapack-linter repository.');
         core.warning(e);
@@ -41,13 +45,13 @@ export async function tryRestoreCache(cacheVersion: number): Promise<boolean> {
 
 export async function saveCache(cacheVersion: number): Promise<void> {
     try {
-        return void await cache.saveCache(cachedfiles, getCacheKeyPrefix(cacheVersion) + context.runId);
+        return void await cache.saveCache(cachedFiles, getCacheKey(cacheVersion, context.runId));
     } catch (e) {
         core.warning('Failed to save the cache. The following errors may be resolved by reporting them in the datapack-linter repository.');
         core.warning(e);
     }
 }
 
-function getCacheKeyPrefix(version: number): string {
-    return `datapack-linter-${context.payload.ref}-${version + privateCacheVersion}-`;
+function getCacheKey(version: number, uniqueID?: number): string {
+    return `datapack-linter-${context.payload.ref}-${version + privateCacheVersion}-${uniqueID ?? ''}`;
 }
