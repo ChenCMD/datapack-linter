9e49d94641bd8cda0385aba8c8fa26a888d8399c
ChenCMD <scnmed.g@gmail.com>
2023-03-17 05:53:49 +0900

    :technologist: `MonoidExtra` を拡張メソッドに

--- src/main/scala/com/github/chencmd/datapacklinter/generic/MonoidExtra.scala
+++ src/main/scala/com/github/chencmd/datapacklinter/generic/MonoidExtra.scala
@@ -1,13 +1,16 @@
 package com.github.chencmd.datapacklinter.generic
 
 import cats.kernel.Monoid
+import cats.MonoidK
 
 object MonoidExtra {
-  def when[A: Monoid](cond: Boolean)(a: => A): A = {
-    if cond then a else Monoid.empty[A]
-  }
+  extension [F[_]: MonoidK, A](action: => F[A]) {
+    def when(cond: Boolean): F[A] = {
+      if cond then action else MonoidK[F].empty[A]
+    }
 
-  def unless[A: Monoid](cond: Boolean)(a: => A): A = {
-    if cond then Monoid.empty[A] else a
+    def unless(cond: Boolean): F[A] = {
+      if cond then MonoidK[F].empty[A] else action
+    }
   }
 }
