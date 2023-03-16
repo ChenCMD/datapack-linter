e24685d73d6eb22a9f66c1def2f18ab2008e5c81
ChenCMD <scnmed.g@gmail.com>
2023-03-17 05:44:42 +0900

    :truck: `CastOps` をgenericパッケージ直下に

--- src/main/scala/com/github/chencmd/datapacklinter/generic/CastOps.scala
+++ src/main/scala/com/github/chencmd/datapacklinter/generic/CastOps.scala
@@ -1,13 +1,14 @@
-package com.github.chencmd.datapacklinter.generic.extensions
+package com.github.chencmd.datapacklinter.generic
 
 import scala.reflect.TypeTest
 
 object CastOps {
   extension [A](value: A) {
-    def downcastOrNone[B](using tt: TypeTest[A, B]): Option[A & B] =
+    def downcastOrNone[B](using tt: TypeTest[A, B]): Option[A & B] = {
       value match {
         case tt(b) => Some(b)
         case _     => None
       }
+    }
   }
 }
