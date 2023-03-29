2cbf53289739ffc1e3474508989b4aa531b9cef8
ChenCMD <scnmed.g@gmail.com>
2023-03-29 17:02:25 +0900

    :construction: `DatapackLanguageService` を作るための諸々を作成

---
+++ src/main/scala/com/github/chencmd/datapacklinter/dls/DLSHelper.scala
@@ -0,0 +1,105 @@
+package com.github.chencmd.datapacklinter.dls
+
+import com.github.chencmd.datapacklinter.ciplatform.CIPlatformInteraction
+import com.github.chencmd.datapacklinter.generic.AsyncExtra
+import com.github.chencmd.datapacklinter.utils.{Datapack, Jsonc}
+
+import cats.Monad
+import cats.data.{OptionT, EitherT}
+import cats.effect.Async
+import cats.implicits.*
+
+import scala.concurrent.duration.*
+
+import scalajs.js
+import js.Dynamic.literal as JSObject
+
+import typings.node.pathMod as path
+import typings.spgodingDatapackLanguageServer.mod as DLS
+import typings.spgodingDatapackLanguageServer.mod.DatapackLanguageService
+import typings.spgodingDatapackLanguageServer.libTypesConfigMod.Config as DLSConfig
+import typings.spgodingDatapackLanguageServer.libPluginsPluginLoaderMod.PluginLoader
+import typings.spgodingDatapackLanguageServer.libTypesVersionInformationMod.VersionInformation
+import typings.spgodingDatapackLanguageServer.libTypesClientCapabilitiesMod as ClientCapabilities
+import typings.spgodingDatapackLanguageServer.libServicesDatapackLanguageServiceMod.DatapackLanguageServiceOptions
+
+object DLSHelper {
+  def createDLS[F[_]: Async](
+    dir: String,
+    dlsConfig: DLSConfig
+  )(using ciInteraction: CIPlatformInteraction[F]): EitherT[F, String, DatapackLanguageService] = {
+    type FOption[A] = EitherT[F, String, A]
+    for {
+      roots <-
+        Datapack.findDatapackRoots[FOption](dir, dlsConfig.env.detectionDepth.asInstanceOf[Int])
+
+      capabilities <- EitherT.pure {
+        ClientCapabilities.getClientCapabilities(
+          JSObject(
+            "workspace" -> JSObject(
+              "configuration"          -> true,
+              "didChangeConfiguration" -> JSObject("dynamicRegistration" -> true)
+            )
+          )
+        )
+      }
+      versionInfo  <- getLatestVersions
+      plugins      <- AsyncExtra.fromPromise[FOption](PluginLoader.load())
+      dls          <- Async[FOption].delay {
+        new DatapackLanguageService(
+          DatapackLanguageServiceOptions()
+            .setCapabilities(capabilities)
+            .setCacheFileUndefined
+            .setGlobalStoragePath(path.join(dir, ".cache"))
+            .setFetchConfig(_ => js.Promise.resolve(dlsConfig))
+            .setPlugins(plugins)
+            .setVersionInformation(versionInfo)
+        )
+      }
+
+      _ <- AsyncExtra.fromPromise[FOption](dls.init())
+      _ <- AsyncExtra.fromPromise[FOption](dls.getVanillaData(dlsConfig))
+      _ <- Async[FOption].delay { dls.roots.push(roots: _*) }
+      _ <- EitherT.liftF(ciInteraction.printInfo("datapack roots:"))
+      _ <- EitherT.liftF(roots.map(_.path).traverse(ciInteraction.printInfo))
+    } yield dls
+  }
+
+  private def getLatestVersions[F[_]: Async](using
+    ciInteraction: CIPlatformInteraction[F]
+  ): EitherT[F, String, VersionInformation] = {
+    type FEither[A] = EitherT[F, String, A]
+
+    inline val VER_INFO_URI    = "https://launchermeta.mojang.com/mc/game/version_manifest.json"
+    inline val PROCESSABLE_VER = "1.16.2"
+
+    for {
+      _               <- EitherT.liftF {
+        ciInteraction.printInfo("[LatestVersions] Fetching the latest versions...")
+      }
+      rawVersionInfo  <- Async[FEither].timeoutTo(
+        AsyncExtra.fromPromise(DLS.requestText(VER_INFO_URI)),
+        7.seconds,
+        EitherT.leftT("[LatestVersions] Fetch timeout")
+      )
+      versionManifest <- EitherT.fromEither {
+        Jsonc
+          .parse(rawVersionInfo)
+          .flatMap(VersionManifest.attemptToVersionManifest)
+          .toRight("[LatestVersions] Failed to parsing version_manifest.json")
+      }
+      ans             <- EitherT.pure {
+        VersionInformation(
+          versionManifest.latest.release,
+          versionManifest.latest.snapshot,
+          versionManifest.versions.reverse
+            .dropWhile(_.id == PROCESSABLE_VER)
+            .map(_.id)
+        )
+      }
+      _               <- EitherT.liftF {
+        ciInteraction.printInfo(s"[LatestVersions] versionInformation = ${js.JSON.stringify(ans)}")
+      }
+    } yield ans
+  }
+}
