56d2776fda7851e0033da474dfcb7c121b0c3634
ChenCMD <scnmed.g@gmail.com>
2023-03-29 17:00:30 +0900

    :construction: `Datapack` util objectを作成

---
+++ src/main/scala/com/github/chencmd/datapacklinter/utils/Datapack.scala
@@ -0,0 +1,32 @@
+package com.github.chencmd.datapacklinter.utils
+
+import com.github.chencmd.datapacklinter.utils.FSAsync
+
+import cats.Monad
+import cats.effect.Async
+import cats.implicits.*
+
+import typings.node.pathMod as path
+import typings.spgodingDatapackLanguageServer.libServicesCommonMod as DLSCommon
+import typings.spgodingDatapackLanguageServer.libTypesHandlersMod.Uri
+
+object Datapack {
+  def findDatapackRoots[F[_]: Async](dir: String, maxDepth: Int): F[List[Uri]] = {
+    for {
+      rootCandidatePaths <- FSAsync.foreachDirRec(dir, dir, _ => true, maxDepth)(_.abs.pure[F])
+      rootPaths          <- {
+        rootCandidatePaths.filterA { p =>
+          for {
+            a <- FSAsync.pathAccessible(path.join(p, "data"))
+            b <- FSAsync.pathAccessible(path.join(p, "pack.mcmeta"))
+          } yield a && b
+        }
+      }
+      rootUris           <- Monad[F].pure {
+        rootPaths
+          .map(Uri.file(_).toString())
+          .map(DLSCommon.getRootUri(_))
+      }
+    } yield rootUris
+  }
+}
