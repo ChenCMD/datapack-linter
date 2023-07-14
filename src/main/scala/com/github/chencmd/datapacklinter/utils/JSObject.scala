9c79e05aff43497a3ac604a37a3ebf8a14584d83
ChenCMD <scnmed.g@gmail.com>
2023-07-14 12:13:32 +0900

    :wrench: JSObject を扱いやすくする

--- src/main/scala/com/github/chencmd/datapacklinter/utils/JSObject.scala
+++ src/main/scala/com/github/chencmd/datapacklinter/utils/JSObject.scala
@@ -1,11 +1,28 @@
 package com.github.chencmd.datapacklinter.utils
 
+import scala.language.dynamics
+
 import scala.scalajs.js
+import scala.scalajs.js.Dynamic
 import scala.scalajs.js.WrappedDictionary
 
-object JSObject {
+import org.scalablytyped.runtime.StringDictionary
+
+object JSObject extends scala.Dynamic {
+  def applyDynamicNamed(name: String)(fields: (String, js.Any)*): js.Object with js.Dynamic = {
+    Dynamic.literal(fields: _*)
+  }
+
+  def applyDynamic(name: String)(fields: (String, js.Any)*): js.Object with js.Dynamic = {
+    Dynamic.literal(fields: _*)
+  }
+
   def isObject(value: js.Any): Boolean = {
     js.typeOf(value) == "object" && value != null
+  }
+
+  def entries[A](obj: StringDictionary[A]): List[(String, A)] = {
+    JSObject.toWrappedDictionary[A](obj).toList
   }
 
   def toWrappedDictionary[A](value: js.Any): WrappedDictionary[A] = {
